1bac3aa20bf088cc23ff7393b5d36ffcdff5b604
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-09-23 08:35:19 -0400

    define and initiate bot

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
-def greet(bot_name):
+def greetings(bot_name):
+    outputLine = f"""===========================================================================""" 
+
+    print(outputLine)
     print("Hello! My name is {0}.".format(bot_name))
-    print("We'd like to help you with your digital marketing needs! We'll help you build your brand online by creating a website, SEO, Social media marketing and more!\nReady to take your business to the next level?")
+    print("We'd like to help you with your digital marketing needs! \nWe'll help you build your brand online by creating a website, SEO, Social media marketing and more!\nReady to take your business to the next level?")
 
 
 def remind_name():
@@ -9,21 +12,20 @@ def remind_name():
     print("What a great name you have, {0}!".format(name))
 
 
-def count():
+def client_query():
     print('Now I want to know how can I help you?')
     services= str(input())
 
-    counter = 0
-    while counter <= services:
-        print("{0} !".format(counter))
-        counter += 1
+    if services == "website":
+        print()
 
-        print(output)
+
+    print(output)
 output = f"""
 ========== Please Select by choosing number ==========
 """
 
-def test():
+def website_offer():
     print("Okay now Can you tell me what type of website you want to build?")
     print("Here is the ordered list: ")
     print("1. Informational ")
@@ -34,7 +36,7 @@ def test():
 
 
 
-    answer = 2
+    answer = 1
     guess = int(input())
     while guess != answer:
         print("Please, try again.")
@@ -52,9 +54,12 @@ def end():
     print('.................................')
     print('.................................')
     input()
+
+
+#Here is my define functions    
     
-greet('Randolfh')  # change it as you need
+greetings('Randolfh')  # change it as you need
 remind_name()
-count()
-test()
+client_query()
+website_offer()
 end()
